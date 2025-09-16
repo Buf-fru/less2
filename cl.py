9cbcca9991bd4c5730a4addc6579aff233f1d52e
Buf-fru <ussbogdan2002@gmail.com>
2025-09-16 15:45:59 +0300

    Update cl.py

--- cl.py
+++ cl.py
@@ -3,7 +3,6 @@ import socket
 
 HOST = ('127.0.0.1', 2234)
 
-# SOCK_DGRAM - UDP,  SOCK_STREAM - TCP, AF_INET - ip v4
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
@@ -11,6 +10,7 @@ sock.connect(HOST)
 f = input()
 
 l = sock.sendall(f.encode())
-#data = sock.recv(1024).decode() # ждем отправки ответа
+
 data = sock.recv(1024)
+
 print(data)
